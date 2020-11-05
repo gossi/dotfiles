384507eb05d4de4e5793c821ad5391898d532519
gossi <gossi@users.noreply.github.com>
2020-11-05 10:36:11 +0100

    update rbenv topic

--- .config/dft/rbenv/install.fish
+++ .config/dft/rbenv/install.fish
@@ -1,3 +1,6 @@
 brew install rbenv
 rbenv init
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+fisher add rbenv/fish-rbenv
+
+set -U fish_user_paths ~/.rbenv/shims ~/.rbenv/bin $fish_user_paths
--- .config/dft/rbenv/update.fish
+++ .config/dft/rbenv/update.fish
@@ -1,1 +1,2 @@
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+#curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+brew upgrade rbenv ruby-build
