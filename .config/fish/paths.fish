23c7e1f6572a41bf17ab89fe59cf6f8ac10eb353
gossi <gossi@users.noreply.github.com>
2022-08-21 19:04:46 +0200

    brew paths for arm and intel arch

--- .config/fish/paths.fish
+++ .config/fish/paths.fish
@@ -1,6 +1,16 @@
 # brew
-pathadd /usr/local/sbin
-pathadd (brew --prefix)/bin
+
+set arch (/usr/bin/uname -m)
+
+# Arm macOS
+if test "$arch" = "arm64"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+
+## Intel macOS
+else
+  eval "$(/usr/local/bin/brew shellenv)"
+
+end
 
 # yarn
 #pathadd (yarn global bin | grep -o '/.*')
