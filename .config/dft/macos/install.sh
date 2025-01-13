638070f5f920b2a7d867b1cf1d71dc8e91b43566
gossi <gossi@users.noreply.github.com>
2025-01-13 09:46:51 +0100

    typing speed

--- .config/dft/macos/install.sh
+++ .config/dft/macos/install.sh
@@ -70,6 +70,10 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
 # Set a blazingly fast keyboard repeat rate
 #defaults write NSGlobalDomain KeyRepeat -int 0
 
